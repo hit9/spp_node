325edf2bd118e4abaeca3284fa7c8c78c2b882fd
hit9 <hit9@icloud.com>
2014-12-28 16:56:41 +0800

    Add test

---
+++ src/c/test.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#ifdef __linux
+#include <mcheck.h>
+#endif
+#include "spp.h"
+
+#define SUIT_CAP 100
+#define CASE(NAME) printf("CASE(" NAME ")")
+#define PASS() printf(" => PASSED\n")
+
+static char *persistent_suite [SUIT_CAP] = {0};
+
+static void assert_handler(spp_t *, char *, size_t, int);
+static void assign_persistent_suite(int, char *);
+
+int main(int argc, const char *argv[])
+{
+#ifdef __linux
+    mtrace();
+#endif
+
+    CASE("spp_new");
+    spp_t * spp = spp_new();
+    spp->handler = &assert_handler;
+    PASS();
+
+    CASE("spp_feed");
+    char *s1 = "2\nok\n4\nbody\n\r\n";
+    assert(spp_feed(spp, s1, strlen(s1)) == SPP_OK);
+    PASS();
+
+    CASE("spp_parse (ok suite)");
+    assign_persistent_suite(0, "ok");
+    assign_persistent_suite(1, "body");
+    assert(spp_parse(spp) == SPP_OK);
+    assert(spp_parse(spp) == SPP_EBADFMT);
+    PASS();
+
+    CASE("spp_parse (bad format suite)");
+    char *s2 = "2\nbadformat\r\n";
+    assert(spp_feed(spp, s2, strlen(s2)) == SPP_OK);
+    assert(spp_parse(spp) == SPP_EBADFMT);
+    PASS();
+
+    CASE("spp_clear");
+    assert(spp_clear(spp) == SPP_OK);
+    PASS();
+
+    CASE("spp_parse (twice)");
+    char *s3 = "2\nok\n";
+    char *s4 = "4\nbody\n\n";
+    assign_persistent_suite(0, "ok");
+    assign_persistent_suite(1, "body");
+    assert(spp_feed(spp, s3, strlen(s3)) == SPP_OK);
+    assert(spp_parse(spp) == SPP_EBADFMT);
+    assert(spp_feed(spp, s4, strlen(s4)) == SPP_OK);
+    assert(spp_parse(spp) == SPP_OK);
+
+    CASE("spp_free");
+    assert(spp_free(spp) == SPP_OK);
+    PASS();
+    return 0;
+}
+
+static void
+assert_handler (spp_t *spp, char *str, size_t size, int idx)
+{
+    assert(memcmp(persistent_suite[idx], str, size) == 0);
+}
+
+
+static void
+assign_persistent_suite(int idx, char *str)
+{
+    persistent_suite[idx] = str;
+}
---
+++ test.js
@@ -0,0 +1,28 @@
+/**
+ * test big buffer
+ */
+
+var util = require('util');
+var spp = require('./build/Release/spp');
+var parser = new spp.Parser();
+
+function randomString(length) {
+  var chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz'
+              .split('');
+
+  if (! length) {
+    length = Math.floor(Math.random() * chars.length);
+  }
+
+  var str = '';
+  for (var i = 0; i < length; i++) {
+    str += chars[Math.floor(Math.random() * chars.length)];
+  }
+  return str;
+}
+
+
+var s = randomString(65535 * 100);
+parser.feed(new Buffer(util.format('%d\n%s\n\n', s.length, s)));
+
+console.log(parser.get());
